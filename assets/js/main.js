87465d546296a50b7608e15a1d1fe9d39a5427a4
audals2478 <audals2478@groupidd.com>
2018-01-08 14:44:57 +0900

    지도수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,15 @@
 //지도표시
-function initialize() {
-    var myLatlng = new google.maps.LatLng(37.544287, 126.871966); // y, x좌표값
-    var mapOptions = {
-        zoom: 15,
-        center: myLatlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-
-    var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map,
-        title: "my home"
+function initMap() {
+    var uluru = {lat: -25.363, lng: 131.044};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: {lat: -34.397, lng: 150.644}
     });
-    var infowindow = new google.maps.InfoWindow(
-        {
-            content: "<h4>my home</h4>",
-            maxWidth: 300
-        }
-    );
-
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(map, marker);
+    var marker = new google.maps.Marker({
+        position: {lat: -34.397, lng: 150.644},
+        map: map
     });
 }
-initialize();
 
 
 
