07bdfefe5ae9f4340a219ebf2d43b02a35aa320e
audals2478 <audals2478@groupidd.com>
2018-01-08 15:43:23 +0900

    수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 //지도표시
 function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
+    var uluru = {lat: 37.544023, lng: 126.872009};
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
+        zoom: 15,
         center: uluru
     });
     var marker = new google.maps.Marker({
