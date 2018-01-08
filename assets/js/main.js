08629bcc060d696bd0ad47c0ee544b36bb1a621e
audals2478 <audals2478@groupidd.com>
2018-01-08 15:31:19 +0900

    지도수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,12 @@ function initMap() {
     var uluru = {lat: -25.363, lng: 131.044};
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 4,
-        center: {lat: -34.397, lng: 150.644}
+        center: uluru
     });
     var marker = new google.maps.Marker({
-        position: {lat: -34.397, lng: 150.644},
+        position: uluru,
         map: map
     });
 }
-
-
-
-
 
 
