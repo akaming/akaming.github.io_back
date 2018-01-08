64357bf3102b87f66b79a9353c87624129882c6a
audals2478 <audals2478@groupidd.com>
2018-01-08 14:18:20 +0900

    지도수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,31 @@
 //지도표시
-document.addEventListener("DOMContentLoaded", function(event) {
-    // - Code to execute when all DOM content is loaded.
-    function initialize() {
-        var myLatlng = new google.maps.LatLng(37.544287, 126.871966); // y, x좌표값
-        var mapOptions = {
-            zoom: 15,
-            center: myLatlng,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-
-        var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            map: map,
-            title: "my home"
-        });
-        var infowindow = new google.maps.InfoWindow(
-            {
-                content: "<h4>my home</h4>",
-                maxWidth: 300
-            }
-        );
-
-        google.maps.event.addListener(marker, 'click', function() {
-            infowindow.open(map, marker);
-        });
+function initialize() {
+    var myLatlng = new google.maps.LatLng(37.544287, 126.871966); // y, x좌표값
+    var mapOptions = {
+        zoom: 15,
+        center: myLatlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     }
 
-    initialize();
-});
+    var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: "my home"
+    });
+    var infowindow = new google.maps.InfoWindow(
+        {
+            content: "<h4>my home</h4>",
+            maxWidth: 300
+        }
+    );
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map, marker);
+    });
+}
+initialize();
+
 
 
 
