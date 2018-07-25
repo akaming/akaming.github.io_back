b4afc14a1aa36b72725395ee0d98e7fae40d45e9
akaming <audals2478@gmail.com>
2018-07-26 04:03:00 +0900

    수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,28 @@
-//지도표시
-function initMap() {
-    var uluru = {lat: 37.544023, lng: 126.872009};
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
-        center: uluru
+
+    //메뉴이동
+    $('a[href^="#"]').click(function(slide){
+        var target = $($(this).attr('href'));
+        if(target.length){
+            slide.preventDefault();
+            $('html,body').animate({
+               scrollTop:target.offset().top 
+            },1000);
+        }
     });
-    var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-    });
-}
+    
+    //지도표시
+    function initMap() {
+        var uluru = {lat: 37.544023, lng: 126.872009};
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 15,
+            center: uluru
+        });
+        var marker = new google.maps.Marker({
+            position: uluru,
+            map: map
+        });
+    }
+
+
 
 
