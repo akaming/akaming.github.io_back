a277f0772d540da2d1177493ff87f20b68cad4aa
akaming <audals2478@gmail.com>
2018-09-04 00:56:00 +0900

    지도 변경
    지도 표시 위치 변경

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,17 @@
 
+     //지도표시
+     function initMap() {
+        var uluru = {lat: 37.544023, lng: 126.872009};
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 15,
+            center: uluru
+        });
+        var marker = new google.maps.Marker({
+            position: uluru,
+            map: map
+        });
+    }
+    
     //메뉴이동
     $('a[href^="#"]').click(function(slide){
         var target = $($(this).attr('href'));
@@ -37,18 +50,7 @@
         }
     
 
-    //지도표시
-    function initMap() {
-        var uluru = {lat: 37.544023, lng: 126.872009};
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 15,
-            center: uluru
-        });
-        var marker = new google.maps.Marker({
-            position: uluru,
-            map: map
-        });
-    }
+   
 
 
 
